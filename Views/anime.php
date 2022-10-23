5d38518f253b2242832ad6f3a47ce6c4b5506519
stefenzzz <phenuthijara@gmail.com>
2022-10-23 11:57:20 -0500

    update

--- Views/anime.php
+++ Views/anime.php
@@ -3,7 +3,7 @@
 <div class="anime-page flex">
    <div class="anime-onepiece">
      <img src="/Views/img/anime/one piece.jpg" alt="">
-     <div class="anime-title"><a href="">One Piece</a></div>
+     <div class="anime-title"><a href="/chapters">One Piece</a></div>
    </div>
 </div>
 </article>
