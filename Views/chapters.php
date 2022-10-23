6e68827baaafe9a7593982e47e0ef2e46e07adc2
stefenzzz <phenuthijara@gmail.com>
2022-10-23 11:32:00 -0500

    update

--- Views/chapters.php
+++ Views/chapters.php
@@ -3,21 +3,25 @@
 <article>
 <?php foreach($data as $d):?>
 
-    <div class="chapter-article flex">
+    <div class="chapter-article grid">
     <a href="/chapter/<?=$d['chapter']?>"><img class="article-chapter-image" src="<?=$d['img']?>" alt="<?=$d['title']?>"></a>
 
-        <div class="article-body">
+        <div class="article-body grid">
 
              <div class="article-title">
              <a href="/chapter/<?=$d['chapter']?>">Chapter <?=$d['chapter'].': '.$d['title']?></a>
+
+                <div class="article-text">
+                    <?=substr($d['text'],0,250)?><a href="/chapter/<?=$d['chapter']?>" class="read-more">...read more</a>
+                    </div>
              </div>
 
-            <div class="article-text">
-                <?=$d['text']?>
-            </div>
-            <div class="article-date">
-                <?=date('M j, Y h:i A',strtotime($d['date']))?>
-            </div>
+           
+                
+                <div class="article-date">
+                    <?=date('M j, Y h:i A',strtotime($d['date']))?>
+                </div>
+            
         </div>
        
     </div>
